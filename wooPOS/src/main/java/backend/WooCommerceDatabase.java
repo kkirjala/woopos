a22505787b6e61674c4cce0585aad81320488aa2
Kalle Kirjalainen <kalle.kirjalainen@gmail.com>
2017-08-13 19:31:40 +0300

    Stub for WooCommerceDB

--- wooPOS/src/main/java/backend/WooCommerceDatabase.java
+++ wooPOS/src/main/java/backend/WooCommerceDatabase.java
@@ -1,5 +1,7 @@
 package backend;
 
+import com.icoderman.woocommerce.WooCommerceAPI;
+import com.icoderman.woocommerce.oauth.OAuthConfig;
 import domain.Order;
 import domain.Product;
 import domain.ShoppingCart;
@@ -10,10 +12,12 @@ import java.util.List;
  *
  * @author kkirjala
  */
-public class WooCommerce implements PosBackend {
+public class WooCommerceDatabase implements PosBackend {
 
     @Override
     public void onPosStartup() {
+        OAuthConfig config = new OAuthConfig("http://woocommerce.com", "consumerKey", "consumerSecret");
+        WooCommerceDatabase wooCommerce = new WooCommerceAPI(config);
     }
 
     @Override
