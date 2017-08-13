92907ae751387a6281b3522a906467d26f13d4df
Kalle Kirjalainen <kalle.kirjalainen@gmail.com>
2017-08-13 22:18:19 +0300

    Fixed indentations (checkstyle error)

--- wooPOS/src/main/java/interfaces/PosBackend.java
+++ wooPOS/src/main/java/interfaces/PosBackend.java
@@ -10,19 +10,19 @@ import java.util.List;
  * @author kkirjala
  */
 public interface PosBackend {
-        
-        public void onPosStartup();
-    
-        public void onPosClose();
-        
-        public List<Product> getProducts();
-        
-        public void addProduct(Product product);
-        
-        public void deleteProduct(Product product);
-        
-        public Order createOrder(ShoppingCart shoppingCart);
-        
-        public Order getOrder(Order order); // TODO: speksaa uusiksi?
-        
+
+    public void onPosStartup();
+
+    public void onPosClose();
+
+    public List<Product> getProducts();
+
+    public void addProduct(Product product);
+
+    public void deleteProduct(Product product);
+
+    public Order createOrder(ShoppingCart shoppingCart);
+
+    public Order getOrder(Order order); // TODO: speksaa uusiksi?
+
 }
