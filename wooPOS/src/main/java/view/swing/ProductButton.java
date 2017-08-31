aecb38caebef7f4310c290a9894c782e45b1aaca
Kalle Kirjalainen <kalle.kirjalainen@gmail.com>
2017-08-31 18:21:19 +0300

    Created a custom JButton for product UI buttons

---
+++ wooPOS/src/main/java/view/swing/ProductButton.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view.swing;
+
+import javax.swing.JButton;
+import model.Product;
+
+/**
+ *
+ * @author kalle
+ */
+public class ProductButton extends JButton {
+    
+    private Product product;
+
+    public ProductButton(Product product) {
+        super(product.getDisplayName());
+        this.product = product;        
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+    
+    
+    
+}
