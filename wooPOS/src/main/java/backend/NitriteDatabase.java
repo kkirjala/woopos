ea9bc18be46d91f3e1045ead599a842f6f51216b
Kalle Kirjalainen <kalle.kirjalainen@gmail.com>
2017-09-02 20:10:52 +0300

    Initialization of the database/REST API moved to constructor from onPosStartUp event. Fixed JavaDoc

--- wooPOS/src/main/java/backend/NitriteDatabase.java
+++ wooPOS/src/main/java/backend/NitriteDatabase.java
@@ -10,39 +10,69 @@ import org.dizitart.no2.Nitrite;
 
 /**
  * A simple local NoSQL database for storing the information needed by WooPOS.
+ *
  * @author kkirjala
  */
 public class NitriteDatabase implements PosBackend {
 
     private Nitrite db;
 
-    public void onPosStartup(WooPOS applicationContext) {
+    /**
+     * A local file woopos.db will contain the Nitrite NoSQL database
+     * information (metadata and data) needed by the application.
+     */
+    public NitriteDatabase() {
         this.db = Nitrite.builder()
                 .compressed()
                 .filePath("woopos.db")
                 .openOrCreate("user", "password");
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    public void onPosStartup(WooPOS applicationContext) {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     public void onPosClose(WooPOS applicationContext) {
         this.db.close();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public List<Product> getProducts() {
         return null;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public void addProduct(Product product) {
 
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public void deleteProduct(Product product) {
 
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public Order createOrder(ShoppingCart shoppingCart) {
         return null;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public Order getOrder(Order order) {
         return null;
     }
--- wooPOS/src/main/java/backend/WooCommerceDatabase.java
+++ wooPOS/src/main/java/backend/WooCommerceDatabase.java
@@ -12,34 +12,63 @@ import java.util.List;
 
 /**
  * This class uses WooCommerce WordPress plugin as the backend of WooPOS.
+ *
  * @author kkirjala
  */
 public class WooCommerceDatabase implements PosBackend {
 
-    public void onPosStartup(WooPOS applicationContext) {
+    /**
+     * Opening a HTTP socket for to access the REST API of a WooCommerce
+     * installation.
+     */
+    public WooCommerceDatabase() {
         OAuthConfig config = new OAuthConfig("http://woocommerce.com", "consumerKey", "consumerSecret");
         WooCommerce wooCommerce = new WooCommerceAPI(config);
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    public void onPosStartup(WooPOS applicationContext) {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     public void onPosClose(WooPOS applicationContext) {
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public List<Product> getProducts() {
         return null;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public void addProduct(Product product) {
 
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public void deleteProduct(Product product) {
 
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public Order createOrder(ShoppingCart shoppingCart) {
         return null;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public Order getOrder(Order order) {
         return null;
     }
